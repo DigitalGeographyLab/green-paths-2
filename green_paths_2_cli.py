5e06f5ad7041dd90c755a8d09323702f2c55aee6
roope heinonen <roope.heinonen@helsinki.fi>
2024-02-20 16:11:50 +0200

    made improvements and added parsers for the cli

--- green_paths_2_cli.py
+++ green_paths_2_cli.py
@@ -1,7 +1,16 @@
 """Cli user interface for Green paths 2."""
 
 import argparse
-from src.preprocessing.main import main as preprocessing_main
+
+from src.logging import setup_logger, LoggerColors
+from src.data.loaders.osm_network_loader import (
+    download_and_move_osm_pbf,
+    get_available_pyrosm_data_sources,
+)
+from src.preprocessing.main import preprocessing
+from src.preprocessing.osm_segmenter import segment_osm_network
+
+LOG = setup_logger(__name__, LoggerColors.BLUE.value)
 
 
 def main():
@@ -12,67 +21,100 @@ def main():
         description="\033[92m Green paths 2 cli user interface \033[0m"
     )
     # Define subparsers for different actions
-    subparsers = parser.add_subparsers(dest="action", help="Available actions 1")
+    subparsers = parser.add_subparsers(dest="action", help="")
 
     # Subparser for preprocessing
     preprocessing_parser = subparsers.add_parser(
         "preprocessing", help="Run the whole preprocessing pipeline."
     )
     preprocessing_parser.add_argument(
+        "-nc",
         "--use_network_cache",
         help="If flag is given, use network cache if found from data/cache directory.",
         action="store_true",
     )
 
     # Subparser for routing
-    routing_parsers = subparsers.add_parser("route", help="Description for action2")
+    routing_parsers = subparsers.add_parser(
+        "route", help="Run the whole routing pipeline."
+    )
     # Add arguments specific to action2 if needed
 
+    # TODO: add cache to routing?
     routing_parsers.add_argument(
-        "--use__test_route",
-        help="Iasdf",
+        "-rc",
+        "--router_cache",
+        help="ROOPE TODO",
         action="store_true",
+    )
+
+    # DATA LOADERS
+    loaders_parser = subparsers.add_parser(
+        "fetch_osm_network", help="Fetch data from different sources."
+    )
+
+    loaders_parser.add_argument(
+        "-a",
+        "--area",
+        type=str,
+        help="Name of city.",
+        required=False,
+    )
+
+    loaders_parser.add_argument(
+        "-l",
+        "--list_available_cities",
+        help="Prints all available cities (pyrosm)",
+        action="store_true",
+        required=False,
+    )
+
+    # OSM PARSER
+    osm_segmenter_parser = subparsers.add_parser(
+        "segment_osm_network", help="Segment OSM network to smaller parts."
+    )
+
+    osm_segmenter_parser.add_argument(
+        "-fp",
+        "--filepath",
+        type=str,
+        help="Filepath to osm network file.",
+        required=True,
+    )
+
+    osm_segmenter_parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        help="Name for the new osm network file, without file extension.",
+        required=False,
     )
 
     args = parser.parse_args()
 
     if args.action == "preprocessing":
-        preprocessing_main(args.use_network_cache)
+        LOG.info("Running preprocessing pipeline. \n\n")
+        preprocessing()
     elif args.action == "route":
-        print("Routing todo...")
+        print("TODO: create routing")
+    elif args.action == "fetch_osm_network":
+        if args.list_available_cities:
+            LOG.info(f"Available cities (pyrosm): \n\n")
+            get_available_pyrosm_data_sources()
+            return
+        elif not args.area:
+            LOG.error(
+                f"Please provide an area name or bounding box coordinates (tuple, minx, miny, maxx, maxy)."
+            )
+        else:
+            LOG.info(f"Fetching OSM network. Using area: {args.area}")
+            download_and_move_osm_pbf(args.area)
+    elif args.action == "segment_osm_network":
+        segment_osm_network(args.filepath, args.name)
     else:
         # print help if no action is given
-
         parser.print_help()
 
 
 if __name__ == "__main__":
     main()
-
-# COLORS FOR CLI
-
-# Light Purple:
-
-# Start Color: \033[95m
-# End Color: \033[0m
-# Green:
-
-# Start Color: \033[92m
-# End Color: \033[0m
-# Blue:
-
-# Start Color: \033[94m
-# End Color: \033[0m
-# Cyan:
-
-# Start Color: \033[96m
-# End Color: \033[0m
-# Orange:
-
-# Unfortunately, there isn't a standard ANSI escape code for orange. ANSI colors are somewhat limited. A close alternative could be a light yellow:
-# Start Color: \033[93m
-# End Color: \033[0m
-# Red:
-
-# Start Color: \033[91m
-# End Color: \033[0m
