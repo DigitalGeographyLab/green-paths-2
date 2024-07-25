099e63dd6618d001edee0a94872235fd7e7a52f8
roope heinonen <roope.heinonen@helsinki.fi>
2024-07-25 12:27:19 +0300

    added logic to see if the segment_store has items when using all -uc (use cache), if it doesnt have we need to run the preprocessing pipeline

--- green_paths_2/src/pipeline_controller.py
+++ green_paths_2/src/pipeline_controller.py
@@ -1,6 +1,7 @@
 """ TODO """
 
-from .config import USER_CONFIG_PATH
+from green_paths_2.src.database_controller import DatabaseController
+from .config import GP2_DB_PATH, SEGMENT_STORE_TABLE, USER_CONFIG_PATH
 from .exposure_analysing.main import exposure_analysing_pipeline
 from .green_paths_exceptions import PipeLineRuntimeError
 from .osm_network_controller import handle_osm_network_process
@@ -38,6 +39,7 @@ def handle_pipelines(pipeline_name: str, use_exposure_cache: bool = False):
         user_config = UserConfig(USER_CONFIG_PATH).parse_config()
         data_handler = UserDataHandler()
         data_handler.populate_data_sources(data_sources=user_config.data_sources)
+        db_controller = DatabaseController(GP2_DB_PATH)
 
         if pipeline_name == "preprocessing":
             osm_network_gdf = handle_osm_network_process(user_config)
@@ -47,30 +49,29 @@ def handle_pipelines(pipeline_name: str, use_exposure_cache: bool = False):
         elif pipeline_name == "analysing":
             exposure_analysing_pipeline(user_config)
         elif pipeline_name == "all":
+
+            skip_preprocessing = False
             if use_exposure_cache:
-                LOG.info("Using exposure cache, skipping preprocessing.")
-                routing_pipeline(data_handler, user_config)
-                LOG.info("\n\n\n * * * \n\n\n")
-                exposure_analysing_pipeline(user_config)
-            else:
+                # make sure that the segment_store table has values
+                if db_controller.get_row_count(SEGMENT_STORE_TABLE) > 0:
+                    skip_preprocessing = True
+                    LOG.info("Using exposure cache, skipping preprocessing.")
+                else:
+                    LOG.info(
+                        "Used -uc (use cache) flag, but nothing found from db segemnt_store, so will process preprocessing pipeline"
+                    )
+
+            if not skip_preprocessing:
                 osm_network_gdf = handle_osm_network_process(user_config)
-                preprocessing_pipeline(  # exposure_gdf, processed_osm_network_gdf =
-                    osm_network_gdf, data_handler, user_config
-                )
+                preprocessing_pipeline(osm_network_gdf, data_handler, user_config)
                 LOG.info("\n\n\n * * * \n\n\n")
 
-                routing_pipeline(
-                    data_handler, user_config  # , exposure_gdf
-                )  # green_paths_route_results_gdf, actual_travel_times_gdf = (
+            routing_pipeline(data_handler, user_config)
 
-                LOG.info("\n\n\n * * * \n\n\n")
-                exposure_analysing_pipeline(
-                    user_config,
-                    # exposure_gdf=exposure_gdf,
-                    # processed_osm_network_gdf=processed_osm_network_gdf,
-                    # routing_results_gdf=green_paths_route_results_gdf,
-                    # actual_travel_times_gdf=actual_travel_times_gdf,
-                )
+            LOG.info("\n\n\n * * * \n\n\n")
+            exposure_analysing_pipeline(
+                user_config,
+            )
     except PipeLineRuntimeError as e:
         LOG.error(f"Pipeline controller failed with error: {e}")
         raise e
